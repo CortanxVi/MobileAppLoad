ab919b86f7168b3e4f1473f5f759df04f737f632
CortanxVi <JirayuP@outlook.com>
2026-01-12 23:23:46 +0700

    deleted ListBox

--- lib/testscreens/profile.dart
+++ lib/testscreens/profile.dart
@@ -7,15 +7,6 @@ class Profile extends StatefulWidget {
   State<Profile> createState() => _ProfileState();
 }
 
-// List images = [ แก้ไข
-//   "https://scontent.fbkk35-1.fna.fbcdn.net/v/t39.30808-6/473620314_1290972855455382_935323258247950810_n.jpg?_nc_cat=100&cb2=99be929b-bd9a46d7&ccb=1-7&_nc_sid=f727a1&_nc_ohc=ovzV2J7lT5IQ7kNvwFVKNoU&_nc_oc=Adm-W4-QGabuSCR7W7LwPfTTxQIbZbay1YGmiqGddpFGnbQfPK0XaI-te_s5orYd1SdNK6WQUlEYOEfGHVcLmEYM&_nc_zt=23&_nc_ht=scontent.fbkk35-1.fna&_nc_gid=XkM2Zcgm-I8v18gvQytJsg&oh=00_AfrokUYCRQI6CnBDiRs6oT6WNodd9CWE2SGTAlw9jZAZQQ&oe=69604669",
-//   "https://scontent.fbkk35-1.fna.fbcdn.net/v/t39.30808-6/561320624_1489836338902365_2130542992296178428_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=108&cb2=99be929b-bd9a46d7&ccb=1-7&_nc_sid=833d8c&_nc_ohc=JTEsSmmWbZoQ7kNvwFHk3RK&_nc_oc=AdkWHDJqEc-JWHIsKSxcDFZxWIY-4cCQnSgZuiiLbuzxKQtVS2zVOBu9cEVdGzYxRtk6806pStG6qx4yrTk6QTi0&_nc_zt=23&_nc_ht=scontent.fbkk35-1.fna&_nc_gid=T8FZ3WpvDFBOoKxz5I-zkg&oh=00_AfpAFgQUO_9JOcI4sxWtmo4x09ccyRcL19cK3p47pc8TpA&oe=69606783",
-//   "https://scontent.fbkk35-1.fna.fbcdn.net/v/t39.30808-6/594077766_1533573967861935_5570257735486701548_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=103&cb2=99be929b-bd9a46d7&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Xyx_iLEn7d4Q7kNvwGfhaO9&_nc_oc=AdnSGt2mxH9y-BrdoneJQuIycJ--yvk_PP1vCE-4e4GI6tqY73k0zNMxd774b2pR3okvSBq0bXN-7A1d8SAYX3Yz&_nc_zt=23&_nc_ht=scontent.fbkk35-1.fna&_nc_gid=QTHUuM9CIqLcObXjR50CBA&oh=00_AfoE8M03lrd34KdcedfLJ24Yx9zPjs438vleQcQcldx7BQ&oe=69604A3A",
-//   "https://scontent.fbkk35-1.fna.fbcdn.net/v/t39.30808-6/480803192_1315870212965646_3796040870942110066_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=110&cb2=99be929b-bd9a46d7&ccb=1-7&_nc_sid=833d8c&_nc_ohc=FEmcw3pkdGIQ7kNvwGc5nTS&_nc_oc=AdnreGHuvExhsgGSUEG16OWNhLupeDbo40t2EphvtVubev_xNsPUeTQXj2XAfWYv_KiQybYgunm7a4EGW4NR_eai&_nc_zt=23&_nc_ht=scontent.fbkk35-1.fna&_nc_gid=Muh79xfbufo2n_lpKgU8ig&oh=00_AfrV93Hg3jGeN_avmGg7Rjclo8dyyuh-zIgdz3InmF7URA&oe=696056B8",
-//   "https://scontent.fbkk35-1.fna.fbcdn.net/v/t39.30808-6/475770384_1302455037640497_4715937107207557619_n.jpg?_nc_cat=103&cb2=99be929b-bd9a46d7&ccb=1-7&_nc_sid=833d8c&_nc_ohc=aWJ_Dyp7wacQ7kNvwHzx1pQ&_nc_oc=AdkK2BGxUNlTFCsqaN223CeL_YukSLgl_XSFMep0M4yq04RHX3Dlo5E9O4KYijNTx85nNvu3JDbOvWaKJmH4OhGU&_nc_zt=23&_nc_ht=scontent.fbkk35-1.fna&_nc_gid=4C2KF8LJ9lMlCNf0y46hYg&oh=00_Afp48dWC4BP_9MHdVB4lv-Ol8cUKNkLgrFoby-9ZIV0xFw&oe=69606584",
-//   "https://scontent.fbkk35-1.fna.fbcdn.net/v/t39.30808-6/470811929_1273074857245182_4063481456134510754_n.jpg?_nc_cat=100&cb2=99be929b-bd9a46d7&ccb=1-7&_nc_sid=833d8c&_nc_ohc=E0iz3XiXDxAQ7kNvwHc6uhc&_nc_oc=AdmMsunjQ-noMlWtcnn7GUBVcF-M3dJUuNmpCqDjQpUihCXtxq1GTTxJ7O5zg4upRRz_Pbw6IwXKp377pDNQ9ORA&_nc_zt=23&_nc_ht=scontent.fbkk35-1.fna&_nc_gid=oQ4ZeUsQ7q0EqZSD3ZJuyA&oh=00_Afr4OU9qsxiEv0HGt81pHFolTr8u91XvOAQeR6e_ViH2vw&oe=69605091",
-// ];
-
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
